34b9bbc4866c5f9ea8a628f153843cc494e2214e
Schorchi <schorchi@gmail.com>
2024-06-06 19:08:53 -0300

    feat: Add Cart component to App.tsx 37.3

---
+++ src/components/Cart/index.tsx
@@ -0,0 +1,51 @@
+import React from 'react'
+import Button from '../Button'
+import {
+  Overlay,
+  CartContainer,
+  CartItem,
+  Quantity,
+  Prices,
+  Sidebar
+} from './styles'
+import starWars from '../../assets/images/star_wars.png'
+import Tag from '../Tag'
+
+const Cart = () => (
+  <CartContainer>
+    <Overlay />
+    <Sidebar>
+      <ul>
+        <CartItem>
+          <img src={starWars} alt="" />
+          <div>
+            <h3>Nome do jogo</h3>
+            <Tag>RPG</Tag>
+            <Tag>PS5</Tag>
+            <span>R$ 150,00</span>
+          </div>
+          <button type="button" />
+        </CartItem>
+        <CartItem>
+          <img src={starWars} alt="" />
+          <div>
+            <h3>Nome do jogo</h3>
+            <Tag>RPG</Tag>
+            <Tag>PS5</Tag>
+            <span>R$ 150,00</span>
+          </div>
+          <button type="button" />
+        </CartItem>
+      </ul>
+      <Quantity>2 jogo(s) no carrinho</Quantity>
+      <Prices>
+        Total de R$ 250,00 <span>Em até 6x sem juros</span>
+      </Prices>
+      <Button title="Clique aqui para continuar com a compra" type="button">
+        Continuar com a compra
+      </Button>
+    </Sidebar>
+  </CartContainer>
+)
+
+export default Cart
