6408af4463e1a466101bb0b7b5e92a4e531e7c65
Schorchi <schorchi@gmail.com>
2024-06-12 18:19:43 -0300

    refactor: Update cart component and store to handle cart opening and closing (37.4 adding and removing products)

--- src/components/Cart/styles.ts
+++ src/components/Cart/styles.ts
@@ -13,16 +13,18 @@ export const Overlay = styled.div`
   background-color: ${cores.preta};
   opacity: 0.7;
 `
-
 export const CartContainer = styled.div`
   position: fixed;
   top: 0;
   left: 0;
   width: 100%;
   height: 100%;
-  display: flex;
+  display: none;
   justify-content: flex-end;
   z-index: 1;
+  &.is-open {
+    display: flex;
+  }
 `
 
 export const Sidebar = styled.aside`
@@ -31,13 +33,11 @@ export const Sidebar = styled.aside`
   padding: 40px 16px 0;
   max-width: 360px;
   width: 100%;
-
   ${ButtonContainer} {
     max-width: 100%;
     width: 100%;
   }
 `
-
 export const Prices = styled.p`
   font-weight: bold;
   font-size: 14px;
@@ -49,44 +49,37 @@ export const Prices = styled.p`
     color: ${cores.cinzaClaro};
   }
 `
-
 export const Quantity = styled.p`
   font-weight: bold;
   font-size: 16px;
   color: ${cores.branca};
-  margin-top: 32px 0 16px;
+  margin: 32px 0 16px;
 `
-
 export const CartItem = styled.li`
   display: flex;
   border-bottom: 1px solid ${cores.cinzaClaro};
   padding: 8px 0;
   position: relative;
-
   img {
-    width: 80px;
     height: 80px;
+    width: 80px;
     object-fit: cover;
     margin-right: 24px;
   }
-
   h3 {
     color: ${cores.branca};
     font-weight: bold;
     font-size: 16px;
   }
-
   span {
     display: block;
-    color: ${cores.branca};
     font-weight: bold;
     font-size: 14px;
+    color: ${cores.branca};
   }
-
   ${TagContainer} {
     margin: 8px 8px 16px 0;
   }
-
   button {
     background-image: url(${fechar});
     width: 16px;
@@ -95,6 +88,6 @@ export const CartItem = styled.li`
     background-color: transparent;
     position: absolute;
     top: 8px;
-    right: 0px;
+    right: 0;
   }
 `
